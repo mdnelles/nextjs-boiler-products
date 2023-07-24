31fb96b6f93d49b1df1c89f7cf672560ce951c2f
Michael Nelles <mxnelles@gmail.com>
2023-07-24 08:18:49 -0500

    update the button action

--- src/app/components/NavButAlignLeft.tsx
+++ src/app/components/NavButAlignLeft.tsx
@@ -1,25 +1,41 @@
+import Link from "next/link";
+import router from "next/router";
 import React from "react";
 
 const ButtonContainer: React.FC = () => {
+   const handleWho = () => {
+      // Route user to /who
+      router.push("/who");
+   };
+
+   const handleContact = () => {
+      // Route user to /contact
+      router.push("/contact");
+   };
+
    return (
       <div className='flex'>
          <button
             className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'
             style={{ width: "100px" }}
+            onClick={handleWho}
          >
-            Button 1
+            Who are we
          </button>
-         <button
-            className='bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded'
-            style={{ width: "100px" }}
-         >
-            Button 2
-         </button>
+         <Link href='/contact'>
+            <button
+               className='bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded'
+               style={{ width: "100px" }}
+            >
+               clients
+            </button>
+         </Link>
          <button
             className='bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded'
             style={{ width: "100px" }}
+            onClick={handleContact}
          >
-            Button 3
+            Contact
          </button>
       </div>
    );
