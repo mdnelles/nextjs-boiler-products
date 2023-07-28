2c0043b4cd716a91ce58b7eb385fd2fa8a0aed18
Michael Nelles <mxnelles@gmail.com>
2023-07-28 11:34:52 -0400

    update for boilerplate

--- src/app/components/NavButAlignLeft.tsx
+++ src/app/components/NavButAlignLeft.tsx
@@ -6,51 +6,31 @@ import React from "react";
 
 const ButtonContainer: React.FC = () => {
    const router = useRouter();
-   const handleWho = () => {
-      // Route user to /who
-      router.push("/who");
-   };
-
-   const handleContact = () => {
-      // Route user to /contact
-      router.push("/contact");
-   };
+   const handleWho = () => router.push("/who");
 
    return (
       <menu>
          <div className='flex space-x-3'>
+            <Link href='/'>
+               <button
+                  className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'
+                  style={{ width: "200px" }}
+               >
+                  Home
+               </button>
+            </Link>
+
             <button
                className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'
+               style={{ width: "200px" }}
                onClick={handleWho}
             >
                Who are we
             </button>
-            <Link href='/clients'>
-               <button
-                  className='bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded'
-                  style={{ width: "200px" }}
-               >
-                  clients
-               </button>
-            </Link>
-            <button
-               className='bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded'
-               style={{ width: "200px" }}
-               onClick={handleContact}
-            >
-               Contact
-            </button>
-            <Link href='/dog'>
-               <button
-                  className='font-bold py-2 px-4 rounded'
-                  style={{ width: "200px" }}
-               >
-                  Dog
-               </button>
-            </Link>
+
             <Link href='/products'>
                <button
-                  className='font-bold py-2 px-4 rounded'
+                  className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'
                   style={{ width: "200px" }}
                >
                   Products
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,35 +1,39 @@
+"use client";
+
 import { prods } from "@/data/dummy";
 import { Product } from "@/types";
 import Image from "next/image";
 
 interface Props {
-   id: number;
+   params: { id: string | number };
+   searchParams?: string | string[] | undefined;
 }
 
-const ProductDisplay: React.FC<Props> = ({ id }) => {
-   const prod = prods.find((p: Product) => p.id === id);
-   console.log("....prod...");
-   console.log(prod);
+const ProductDisplay: React.FC<Props> = ({ params }) => {
+   const id = params && params.id ? params.id : 1;
+   const numericId = typeof id === "string" ? +id : 1;
+   const prod = prods.filter((p: Product) => p.id === numericId);
+
    return (
       <>
          <div className='container'>
             <div className='cell'>
-               {prod?.title}
+               {prod[0] ? prod[0].title : "title"}
                <Image
-                  src={prod?.thumbnail || "/noimage.png"}
+                  src={prod[0] ? prod[0].thumbnail : "/noimage.png"}
                   width={100}
                   height={100}
-                  alt={prod?.title || "no title"}
+                  alt={prod[0] ? prod[0].title : "no title"}
                />
             </div>
             <div className='cell'>
                <ul>
-                  <li>Price ${prod?.price}</li>
-                  <li>Discount % {prod?.discountPercentage}</li>
-                  <li>Rating: {prod?.rating}</li>
-                  <li>In Stock: {prod?.stock}</li>
-                  <li>Brand: {prod?.brand}</li>
-                  <li>Category: {prod?.category}</li>
+                  <li>Price ${prod[0] ? prod[0].price : 0}</li>
+                  <li>Discount % {prod[0] ? prod[0].discountPercentage : 0}</li>
+                  <li>Rating: {prod[0] ? prod[0].rating : 0}</li>
+                  <li>In Stock: {prod[0] ? prod[0].stock : 0}</li>
+                  <li>Brand: {prod[0] ? prod[0].brand : 0}</li>
+                  <li>Category: {prod[0] ? prod[0].category : 0}</li>
                </ul>
             </div>
             <div className='cell'>
